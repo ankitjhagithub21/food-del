a97b061cc4f2b5ccc70e21f22d27fca245f8e0fa
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-21 13:07:43 +0530

    fix foodslice

--- frontend/src/redux/slices/foodSlice.js
+++ frontend/src/redux/slices/foodSlice.js
@@ -3,13 +3,15 @@ import { createSlice } from '@reduxjs/toolkit'
 export const foodSlice = createSlice({
   name: 'food',
   initialState:{
-   
+    value:[],
     category:"Beef",
     
   },
   reducers: {
    
-   
+    setAllFood: (state, action) => {
+      state.value = action.payload
+    },
     setCategory:(state,action)=>{
       state.category = action.payload
     },
