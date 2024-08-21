45dc19d23d3e29bf5dd797ee1a79955dd55b0215
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-21 12:55:28 +0530

    changes footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,10 @@ const Footer = () => {
             <div className='container md:p-5 p-2 mx-auto flex flex-wrap'>
                 <div className='lg:w-1/3 md:w-1/2 w-full flex p-3 flex-col gap-3'>
                     <Link to={"/"} className='text-3xl font-bold'>MealMate</Link>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem at hic dicta placeat atque doloribus nostrum eveniet amet alias, laboriosam in quos quod nulla obcaecati deserunt asperiores molestiae. Est!</p>
+                    <p>   Satisfy your cravings with a wide variety of delicious meals, prepared
+                        fresh and delivered straight to your door. From comfort food to gourmet
+                        delights, we've got something for everyone. Place your order now and enjoy
+                        the taste of happiness!</p>
                     <div className='flex items-center gap-3 '>
                         <CiFacebook size={25} />
                         <CiTwitter size={25} />
